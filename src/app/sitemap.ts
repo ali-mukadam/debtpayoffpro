34c796d57e1b49e5809c5c32553723c6a37e007f
Usman <alishaikh3355@gmail.com>
2025-09-26 21:58:09 +0530

    indexing issue fix 2

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://debtpayoffpro.com',
+      url: 'https://www.debtpayoffpro.com',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1.0,
